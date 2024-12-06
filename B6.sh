1f3c74ab90d36e01e339be250c02edd3e63300e1
rajaneeshp <rbparaddi90@gmail.com>
2024-12-06 11:37:07 +0530

    Resolved merge conflict and kept stashed content

--- B6.sh
+++ B6.sh
@@ -1,21 +1,3 @@
-#!/bin/bash			
 
-# raj.................
-set -x
-echo "to find biggest of 3 numbers"
-echo ""
-# echo "" added to leave a space in output
-if [ $# -ne 3 ];
-then
-        echo "Rajaneesh arguments should be equal to 3"
-        exit 1
-fi
-# above fi to end the 3 arguments stmt
+# i removed everything which was full of bullshit
 
-if [[ $1 -eq $2 && $1 -eq $3 ]]; then
-	echo "all are equal"
-	echo "Rajaneesh"
-# to find equality, if all are equal program will end here with echo output
-
-
-fi
